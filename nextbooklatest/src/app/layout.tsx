0e176bbad66ca65b5dd5ec38f7079c86a51ece37
SSmln <lms7630@gmail.com>
2024-02-03 19:43:06 +0900

    -2

---
+++ nextbooklatest/src/app/layout.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { createGlobalStyle } from "styled-components";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+a {
+  cursor: pointer;
+  text-decoration: none;
+  transition: .25s;
+  color: #000
+}
+
+ol, ul {
+  list-style: none;
+}
+`
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    
+    <html lang="en">
+    <head>
+      <meta key="charset" name="charset" content="utf-8" />
+      <meta 
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+      /> 
+      <meta property="og:locale" content="ko_KR" />
+      <meta property="og:type" content="website" />
+
+    </head>
+    <body className={inter.className}>
+      <GlobalStyle />
+      {children}
+      </body>
+    </html>
+  );
+}
